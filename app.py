66d772bdbeae6111c2109b587de1bcd7a1ada0c9
Lior <lior4007@gmail.com>
2023-11-06 14:10:46 +0200

    Intergrate db with python & add docker file & add db class

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from logs.logging import logger
+from config.config import DB_CONFIG
+from database.PostgreSQL import PostgreSQL
 
 logger.info("The app is running...")
+print("The app is running...")
+db = PostgreSQL(**DB_CONFIG)
+db.connect()
+db.create_tables()
+db.save_user_to_db('Lior', '0501234567','lior@gmail.com')
+db.disconnect()
+
 from bot import init_telegram_bot  # run telegram bot server
